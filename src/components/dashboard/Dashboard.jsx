33beb379c0ba3c17318e08837c6ea470dbf858c9
sakshamak <saksham.8484@gmail.com>
2022-07-30 03:14:56 +0530

    dashboard created

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,17 @@
-
+import style from "./Dashboard.module.css"
 export const Dashboard = () => {
 
     return (
-        <div className = "my-4 py-1 px-2">
-            <h1>heelo</h1>
+        <div className={`${style["dashboard-container"]} my-4 p-2`}>
+            <aside className={`${style["sideNav"]}`}>
+                sideNav
+            </aside>
+            <main classname={style["user-feed"]}>
+                hello world
+            </main>
+            <aside className={`${style["suggestions"]}`}>
+                suggestions
+            </aside>
         </div>
     )
 }
