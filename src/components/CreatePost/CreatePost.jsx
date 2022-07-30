f6a02b94c55c53ab2a3eaf31ade58198f9669c14
sakshamak <saksham.8484@gmail.com>
2022-07-30 15:35:13 +0530

    feat: emoji keyboard added

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,16 +1,39 @@
 import style from './CreatePost.module.css'
 import { usePost } from '../../context/PostContext';
+import { useEffect } from 'react';
+import Picker from 'emoji-picker-react';
 
 export const CreatePost = () => {
-    const {chosenMedia, setMedia} = usePost();
+    const { chosenMedia, setMedia, src, setSrc, getEmoji, postText, setPostText } = usePost();
+    const mediaCount = Object.values(chosenMedia);
+    const imageExist = mediaCount.length !== 0;
+    
+
+    useEffect(() => {
+        setSrc(mediaCount.map(item => URL.createObjectURL(item)))
+    }, [chosenMedia])
+
+    const deleteImage = (idx) => { console.log(src); return setSrc(src.filter(item => item !== src[idx])) }
+
+
     return (
         <div className={style["post-container"]}>
-            <textarea className={style["write-post"]}></textarea>
+            <textarea className={style["write-post"]} cols="5" value = {postText} onChange = {e => setPostText(e.target.value)} ></textarea>
             <div className={style["add-to-post"]}>
-                <input type="file" accept="video/*, image/*" capture="environment" multiple id="uploadImage" className={style["uploadImage"]} onChange={e => setMedia(e.target.files)} /><label htmlFor="uploadImage"><span className="material-symbols-outlined">image</span></label>
-                <span className="material-symbols-outlined">gif_box</span>
-                <span className="material-symbols-outlined">sentiment_satisfied</span>
-                {Object.keys(chosenMedia).length !== 0 && <img src={URL.createObjectURL(chosenMedia[0])} alt="{" />}
+                <div className={style["upload-items"]}>
+                    <input type="file" accept="video/*, image/*" capture="environment" multiple id="uploadImage" className={style["uploadImage"]} onChange={e => setMedia(e.target.files)} /><label htmlFor="uploadImage"><span className="material-symbols-outlined">image</span></label>
+                    <span className="material-symbols-outlined">gif_box</span>
+                    <span className="material-symbols-outlined">sentiment_satisfied</span>
+                </div>
+                <div className={style["preview-image"]}>
+                    {imageExist && src?.map((item, idx) => (
+                        <div>
+                            <i className={`material-icons ${style["delete-image"]}`} onClick={() => deleteImage(idx)} >close</i>
+                            <img className={style["uploaded-image"]} src={src[idx]} alt="media" />
+                        </div>
+                    ))}
+                </div>
+                <Picker onEmojiClick={getEmoji}/>
             </div>
         </div>
     )
--- src/context/PostContext.js
+++ src/context/PostContext.js
@@ -6,9 +6,14 @@ const usePost = () => useContext(PostContext);
 
 const PostProvider = ({ children }) => {
     const [chosenMedia, setMedia] = useState({});
+    const [src, setSrc] = useState([])
+    const [postText, setPostText] = useState("");
+    const getEmoji = (event, emojiObject) => {
+        setPostText(prev => prev + emojiObject.emoji);
+    };
 
     return (
-        <PostContext.Provider value={{ chosenMedia, setMedia }}>
+        <PostContext.Provider value={{ chosenMedia, setMedia, src, setSrc, getEmoji, postText, setPostText }}>
             {children}
         </PostContext.Provider>
     )
